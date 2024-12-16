5089049d21135605f14c2fed0085d7cd46095f92
naufalbayazid <naufalichwanbayazid@gmail.com>
2024-12-16 14:40:19 +0700

    menambahkan AlamatNavigasi ke home

--- app/src/main/java/com/example/rommdatabase/ui/theme/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/rommdatabase/ui/theme/navigation/AlamatNavigasi.kt
@@ -3,3 +3,6 @@ package com.example.rommdatabase.ui.theme.navigation
 interface AlamatNavigasi {
     val route : String
 }
+object DestinasiHome : AlamatNavigasi{
+    override val route = "home"
+}
