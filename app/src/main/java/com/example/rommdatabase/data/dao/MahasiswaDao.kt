0887f18f555b319697028dea2860c2f9e82f065e
naufalbayazid <naufalichwanbayazid@gmail.com>
2024-12-16 14:02:28 +0700

    menambahkan fungsi getAllMahasiswa

--- app/src/main/java/com/example/rommdatabase/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/rommdatabase/data/dao/MahasiswaDao.kt
@@ -4,11 +4,24 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import com.example.rommdatabase.data.entity.Mahasiswa
+import com.example.rommdatabase.repository.RepositoryMhs
+import kotlinx.coroutines.flow.Flow
+
 @Dao
 interface MahasiswaDao {
     @Insert
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
 }
 
-//Get All Mahasiswa
-@Query("SELECT * FROM mahasiswa ORDER by nama ASC")
+class LocalRepositoryMhs
+    (private val mahasiswaDao: MahasiswaDao) :RepositoryMhs{
+        override suspend fun
+                insertMhs(mahasiswa: Mahasiswa) {
+
+                    mahasiswaDao.insertMahasiswa(mahasiswa)
+        }
+    override fun getAllMhs():Flow<List<Mahasiswa>>{
+        return
+        mahasiswaDao.getAllMahasiswa
+    }
+}
