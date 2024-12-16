a04fbb8b7bef0ba1603e65fe79ae2034a0e7b776
naufalbayazid <naufalichwanbayazid@gmail.com>
2024-12-16 14:50:58 +0700

    membuat class baru HomeMhsViewModel.kt

---
+++ app/src/main/java/com/example/rommdatabase/ui/viewmodel/HomeMhsViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.rommdatabase.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.rommdatabase.repository.RepositoryMhs
+
+class HomeMhsViewModel(
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel () {
+
+}
