1624120fc22112cba85abfc460a34bd9a6f8d1f3
naufalbayazid <naufalichwanbayazid@gmail.com>
2024-12-16 16:33:33 +0700

    membuat class baru DetailMhsViewModel.kt dan mengambil dari repositoryMhs.

--- app/src/main/java/com/example/rommdatabase/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/rommdatabase/ui/viewmodel/DetailMhsViewModel.kt
@@ -1,5 +1,15 @@
 package com.example.rommdatabase.ui.viewmodel
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.rommdatabase.repository.RepositoryMhs
+
+class DetailMhsViewModel (
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoryMhs,
+) : ViewModel() {
+
+}
 data class DetailUiState(
     val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
     val isLoading: Boolean = false,
