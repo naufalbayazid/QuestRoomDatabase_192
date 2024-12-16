982eb03c99e0907e7fe138191d55a8e91b8166bd
naufalbayazid <naufalichwanbayazid@gmail.com>
2024-12-16 18:45:02 +0700

    mengisi parameter di Mhsview

--- app/src/main/java/com/example/rommdatabase/ui/viewMahasiswa/HomeMhsView.kt
+++ app/src/main/java/com/example/rommdatabase/ui/viewMahasiswa/HomeMhsView.kt
@@ -1,8 +1,14 @@
 package com.example.rommdatabase.ui.viewMahasiswa
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.rommdatabase.data.entity.Mahasiswa
 
 @Composable
 fun CardMhs(
+    mhs:Mahasiswa,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = { }
+) {
+}
 
-)
