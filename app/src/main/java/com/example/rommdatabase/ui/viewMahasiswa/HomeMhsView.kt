33798d092ee4f4d0c77a74b42148189f0b9ea096
naufalbayazid <naufalichwanbayazid@gmail.com>
2024-12-16 18:59:56 +0700

    mengisi Column Row beserta mengisi data text

--- app/src/main/java/com/example/rommdatabase/ui/viewMahasiswa/HomeMhsView.kt
+++ app/src/main/java/com/example/rommdatabase/ui/viewMahasiswa/HomeMhsView.kt
@@ -1,7 +1,22 @@
 package com.example.rommdatabase.ui.viewMahasiswa
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType.Companion.Text
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.rommdatabase.data.entity.Mahasiswa
 
 @Composable
@@ -10,5 +25,37 @@ fun CardMhs(
     modifier: Modifier = Modifier,
     onClick: () -> Unit = { }
 ) {
+    Card(
+        onClick = onClick,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Column (
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Row (
+                modifier = modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+
+            ){
+                Icon(imageVector = Icons.Filled.Person, contentDescription = "")
+                Spacer(modifier = Modifier.padding(4.dp))
+                Text(
+                    text = mhs.nama,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp
+                )
+
+
+
+
+
+
+
+
+            }
+        }
+    }
 }
 
