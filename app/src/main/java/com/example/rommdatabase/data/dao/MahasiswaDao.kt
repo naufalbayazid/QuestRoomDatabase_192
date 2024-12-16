0ddddb48606e66494a31fd17cd69841b6958aea9
naufalbayazid <naufalichwanbayazid@gmail.com>
2024-12-16 14:09:12 +0700

    menambahkan fungsi getAllMahasiswa

--- app/src/main/java/com/example/rommdatabase/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/rommdatabase/data/dao/MahasiswaDao.kt
@@ -11,17 +11,18 @@ import kotlinx.coroutines.flow.Flow
 interface MahasiswaDao {
     @Insert
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+    abstract fun getAllMahasiswa(): Flow<List<Mahasiswa>>
 }
 
 class LocalRepositoryMhs
-    (private val mahasiswaDao: MahasiswaDao) :RepositoryMhs{
-        override suspend fun
-                insertMhs(mahasiswa: Mahasiswa) {
-
-                    mahasiswaDao.insertMahasiswa(mahasiswa)
-        }
-    override fun getAllMhs():Flow<List<Mahasiswa>>{
-        return
-        mahasiswaDao.getAllMahasiswa
+    (private val mahasiswaDao: MahasiswaDao)  : RepositoryMhs
+{
+    override suspend fun insertMhs(mahasiswa: Mahasiswa) {
+        mahasiswaDao.insertMahasiswa(mahasiswa)
     }
-}
+
+    override fun getAllMhs(): Flow<List<Mahasiswa>> {
+        return mahasiswaDao.getAllMahasiswa()
+    }
+
+  
--- app/src/main/java/com/example/rommdatabase/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/rommdatabase/repository/RepositoryMhs.kt
@@ -1,7 +1,9 @@
 package com.example.rommdatabase.repository
 
 import com.example.rommdatabase.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+    fun getAllMhs(): Flow<List<Mahasiswa>>
 }
